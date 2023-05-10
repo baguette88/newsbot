6c2542aee684b169e6354d1f2332af67fab4de7e
jbrown7407 <jbrown7407@gmail.com>
2023-05-09 22:57:48 -0400

    x
    x

--- simulation.js
+++ simulation.js
@@ -1,5 +1,8 @@
 window.onload = function() {
 let pauseSimulation = false;
+let deaths = 0;
+let livingAnimals=0
+
 
 let timeScale = 1;
 const speedSlider = document.getElementById('speed-slider');
@@ -9,24 +12,26 @@ speedSlider.addEventListener('input', function() {
 
 
 class Animal {
-    constructor(x, y, generation = 0, speed = Math.min(Math.floor(Math.random() * 5) + 1, 5), iq = Math.floor(Math.random() * 10) + 1, efficiency = Math.min(Math.floor(Math.random() * 5) + 1, 3)) {
-        console.log("Animal created at position: ", x, y);
+    constructor(x, y, color, level = 0, offspring = 0, speed = Math.min(Math.floor(Math.random() * 5) + 1, 5), iq = Math.floor(Math.random() * 10) + 1, efficiency = Math.min(Math.floor(Math.random() * 5) + 1, 3)) {
         this.x = x;
         this.y = y;
         this.size = 10;
-        this.food = 100;
+        this.food = 50;
         this.dead = false;
-        this.generation = generation;
+        this.generation = offspring; // Changed here
         this.speed = speed;
         this.iq = iq;
         this.efficiency = efficiency;
         this.deathTime = null;
-        // Generate a random color for the animal
-        this.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        this.color = color || `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        this.level = level;
+        this.reproduceCooldown = 0;
+        this.hasEaten = false;
+        this.offspring = offspring;  
+        livingAnimals++;  
     }
 
     draw() {
-        console.log("Drawing animal at position: ", this.x, this.y);
         const radius = this.dead ? this.size / 2 : this.size;
         ctx.fillStyle = this.dead ? 'gray' : this.color;
         ctx.strokeStyle = this.dead ? 'gray' : 'black';
@@ -35,12 +40,20 @@ class Animal {
         ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI);
         ctx.fill();
         ctx.stroke();
-    }
 
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+        ctx.fillText(this.level, this.x, this.y + 5);
+    }
 
 
     move(foodItems) {
         if (!this.dead) {
+            let newX = this.x;
+            let newY = this.y;
+    
+            const angle = Math.random() * 2 * Math.PI; // define angle here
+    
             if (Math.random() < this.iq / 10 && foodItems.length > 0) {
                 const nearestFood = foodItems.reduce((nearest, food) => {
                     const d = Math.sqrt((food.x - this.x) ** 2 + (food.y - this.y) ** 2);
@@ -49,24 +62,38 @@ class Animal {
                 const dx = nearestFood.x - this.x;
                 const dy = nearestFood.y - this.y;
                 const d = Math.sqrt(dx ** 2 + dy ** 2);
-                this.x += 0.5 * this.speed * dx / d * timeScale;
-                this.y += 0.5 * this.speed * dy / d * timeScale;
+                newX += 0.5 * this.speed * dx / d * timeScale;
+                newY += 0.5 * this.speed * dy / d * timeScale;
             } else {
-                const angle = Math.random() * 2 * Math.PI;
-                this.x += 0.5 * this.speed * Math.cos(angle) * timeScale;
-                this.y += 0.5 * this.speed * Math.sin(angle) * timeScale;
+                newX += 0.5 * this.speed * Math.cos(angle) * timeScale;
+                newY += 0.5 * this.speed * Math.sin(angle) * timeScale;
             }
-            this.x = Math.max(this.size, Math.min(canvas.width - this.size, this.x));
-            this.y = Math.max(this.size, Math.min(canvas.height - this.size, this.y));
+    
+            // check if new position is inside the canvas
+            if (newX - this.size < 0 || newX + this.size > canvas.width) {
+                // bounce off the wall by reversing x direction
+                newX = this.x - 0.5 * this.speed * Math.cos(angle) * timeScale;
+            }
+            if (newY - this.size < 0 || newY + this.size > canvas.height) {
+                // bounce off the wall by reversing y direction
+                newY = this.y - 0.5 * this.speed * Math.sin(angle) * timeScale;
+            }
+    
+            // update position
+            this.x = newX;
+            this.y = newY;
         }
     }
     
+    
+    
 
-    consume(foodItems, carcasses) {
+    consume(foodItems, carcasses, animals) {
         if (!this.dead) {
             foodItems.forEach((food, index) => {
                 if (Math.sqrt((food.x - this.x) ** 2 + (food.y - this.y) ** 2) < this.size + food.size) {
                     this.food++;
+                    this.hasEaten = true;
                     foodItems.splice(index, 1);
                 }
             });
@@ -76,16 +103,23 @@ class Animal {
                     carcasses.splice(index, 1);
                 }
             });
+            animals.forEach((animal, index) => {
+                if (!animal.dead && this.level >= animal.level + 2 && this.color !== animal.color) {
+                    // ... the rest of your consumption code ...
+                    animal.dead = true;
+                    if (!animal.dead) {
+                        livingAnimals--;  // Decrement living animals counter
+                    }
+                    deaths += 1;  // Increment deaths counter only if the animal was alive
+                }
+            });
         }
     }
+    
 
     draw() {
-        console.log("Drawing animal at position: ", this.x, this.y);
         const radius = this.dead ? this.size / 2 : this.size;
-        const gradient = ctx.createRadialGradient(this.x, this.y, radius / 5, this.x, this.y, radius);
-        gradient.addColorStop(0, `hsl(${this.generation * 50}, 50%, 50%)`);
-        gradient.addColorStop(1, 'white');
-        ctx.fillStyle = this.dead ? 'gray' : gradient;
+        ctx.fillStyle = this.dead ? 'gray' : this.color;
         ctx.strokeStyle = this.dead ? 'gray' : 'black';
         ctx.lineWidth = 2;
         ctx.beginPath();
@@ -100,25 +134,37 @@ class Animal {
                 // Some logic here to update the animal's state
                 // For example, you might decrease the animal's food supply:
                 this.food -= this.efficiency * timeScale / 10;
-        
+                let newLevel = Math.floor(this.food / 20);
+                if (newLevel > this.level) {
+                    // Level up and grow by 10%
+                    this.level = newLevel;
+                    this.size *= 1.1;
+                }
+                this.level = Math.floor(this.food / 20);
+                this.reproduceCooldown = Math.max(0, this.reproduceCooldown - timeScale);
                 // If the animal's food supply is depleted, it dies
-                if (this.food <= 0) {
+                if (this.food <= 0 && !this.dead) {  // Check if the animal is not already dead
                     this.dead = true;
+                    livingAnimals--;  // Decrement living animals counter
                     this.deathTime = Date.now();
                 }
             
         
      // If the animal has enough food, it reproduces
-     if (this.food >= 85) { // Increased the minimum food threshold
-        this.food -= 20;
+     if (this.food >= 85 && this.reproduceCooldown <= 0 && this.hasEaten) {
+        this.food -= 40;
+        this.reproduceCooldown = 3;  // 3 seconds cooldown
+        this.hasEaten = false;
         const x = this.x + Math.random() * 20 - 10;
         const y = this.y + Math.random() * 20 - 10;
         const generation = this.generation + 1;
-        const speed = Math.max(1, this.speed + Math.ceil(Math.random() * 3) *.0008);
+        const speed = Math.max(1, this.speed + Math.ceil(Math.random() * 3) - 1);
         const iq = Math.max(1, this.iq + Math.ceil(Math.random() * 3) - 1);
         const efficiency = Math.max(1, this.efficiency + Math.ceil(Math.random() * 3) - 1);
-        return new Animal(x, y, generation, speed, iq, efficiency);
-    }   
+        return new Animal(x, y, this.color, this.level, generation, speed, iq, efficiency);
+    }
+
+
     return null;
             }
         }
@@ -142,23 +188,26 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
+
+let animals = [];
+let colors = ['red', 'orange', 'yellow', 'blue', 'indigo', 'violet', 'dark green', 'black', 'white', 'pink'];
+
+
 function restart() {
     pauseSimulation = false;
     animals = [];
     foodItems = [];
-    for (let i = 0; i < 50; i++) {
-        const x = Math.random() * (canvas.width - 20) + 10;  // Adjusted
-        const y = Math.random() * (canvas.height - 20) + 10; // Adjusted
-        animals.push(new Animal(x, y));
+    livingAnimals=0;
+    deaths=0;
+    for (let i = 0; i < 10; i++) {
+        const x = Math.random() * (canvas.width - 2 * Animal.size) + Animal.size;
+        const y = Math.random() * (canvas.height - 2 * Animal.size) + Animal.size;
+        const colorIndex = i % colors.length;
+        animals.push(new Animal(x, y, colors[colorIndex]));
     }
 }
     
-    let animals = [];
-    for (let i = 0; i < 25; i++) {
-        const x = Math.random() * canvas.width;
-        const y = Math.random() * canvas.height;
-        animals.push(new Animal(x, y));
-    }
+
     
     let foodItems = [];
     
@@ -173,12 +222,15 @@ function restart() {
         const animalCount = animals.length;
         const foodCount = foodItems.length;
         let maxGeneration = 0;
+        let maxOffspring = 0;
         if (animalCount > 0) {
             maxGeneration = Math.max(...animals.map(animal => animal.generation));
+            maxOffspring = Math.max(...animals.map(animal => animal.offspring));
         }
     
-        statusBar.innerHTML = `Animals: ${animalCount} | Food: ${foodCount} | Generations: ${maxGeneration}`;
+        statusBar.innerHTML = `Animals: ${livingAnimals} | Deaths: ${deaths} | Food: ${foodCount} | Offspring: ${maxOffspring}`; 
     }
+    
     
     function removeDeadAnimals() {
         const now = Date.now();
@@ -200,7 +252,7 @@ function restart() {
     
             animals.forEach(animal => {
                 animal.move(foodItems);
-                animal.consume(foodItems, animals);
+                animal.consume(foodItems, [], animals); // Pass the animals array here
                 const offspring = animal.update(timeScale);
                 if (offspring) {
                     animals.push(offspring);
@@ -228,7 +280,7 @@ function restart() {
         pauseSimulation = false;
         animals = [];
         foodItems = [];
-        for (let i = 0; i < 50; i++) {
+        for (let i = 0; i < 10; i++) {
             const x = Math.random() * canvas.width;
             const y = Math.random() * canvas.height;
             animals.push(new Animal(x, y));
@@ -242,10 +294,11 @@ function restart() {
 
     document.getElementById('pause-button').addEventListener('click', pause);
     restart(); 
-    window.addEventListener('resize', function () {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    });
+
+   window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 
     document.getElementById('spawn-animals').addEventListener('click', function() {
         for (let i = 0; i < 5; i++) {
